def61b44d0c78c1b536a71d840a6c80dac37adb5
yowjie <yeojbongon@gmail.com>
2025-01-23 11:31:37 +0800

    01-23-2025

---
+++ F1.py
@@ -0,0 +1,24 @@
+first_name = input("Enter First name: ")
+
+last_name = input("Enter Last name: ")
+
+middle_name = input("Enter Middle name: ")
+
+bday = input("Enter Birthday: ")
+
+gen = input("Enter Gender: ")
+
+add = input("Enter Address: ")
+
+course = input("Enter Course: ")
+
+year = input("Enter Year Level: ")
+
+print("\nFirst Name:", first_name)
+print("Last Name: ", last_name)
+print("Middle Name: ", middle_name)
+print("Birthday: ", bday)
+print("Gender: ", gen)
+print("Address: ", add)
+print("Course: ", course)
+print("Year level: ", year) 
