2c3d67ec39a2eac9cc511532bffd8bbe33ba35d3
yowjie <yeojbongon@gmail.com>
2025-01-30 16:11:51 +0800

    01-30-2025

--- TA1/Program1.py
+++ TA1/Program1.py
@@ -1,20 +1,22 @@
-word = input("Enter a string: ")
+text = input("Enter a string: ")
 
-#vowels
-vowels = ["a", "e", "i", "o", "u"]
-count = 0
-for character in word:
-    if character in vowels:
-        count += 1
+vowels = "aeiouAEIOU"
+count_vowels = 0
+count_consonants = 0
+count_spaces = 0
+count_others = 0
 
-#consonant
-vowels = ["a", "e", "i", "o", "u"]
-count1 = 0
-for character in word:
-    if character in vowels:
-        count += 1
-    elif character in vowels:
-        c
+for char in text:
+    if char in vowels:
+        count_vowels += 1
+    elif char in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ":  
+        count_consonants += 1
+    elif char == " ": 
+        count_spaces += 1
+    else:  
+        count_others += 1
 
-print("Vowels:", count)
-print("Consonant:", count1)
+print("Vowels:", count_vowels)
+print("Consonant:", count_consonants)
+print("Spaces:", count_spaces)
+print("Other characters:", count_others)
