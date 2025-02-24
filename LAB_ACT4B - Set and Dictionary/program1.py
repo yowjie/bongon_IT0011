9e300d4fcb61de2d55da2567eb68a51ff5730605
yowjie <yeojbongon@gmail.com>
2025-02-24 22:49:11 +0800

    02-24-2025

---
+++ LAB_ACT4B - Set and Dictionary/program1.py
@@ -0,0 +1,28 @@
+A = {'a', 'b', 'c', 'd', 'f', 'g', 'h'}
+B = {'b', 'c', 'h', 'l', 'm', 'o'}
+C = {'c', 'd', 'f', 'h', 'i', 'j', 'k'}
+
+union_A_B = A | B
+print("a. How many elements are there in set A and B?")
+print(len(union_A_B), union_A_B)
+print()
+
+B_not_A_C = B - (A | C)
+print("b. How many elements are there in B that is not part of A and C?")
+print(len(B_not_A_C), B_not_A_C)
+print()
+
+print("c. Show the following using set operations:")
+hijk = (B & A) | C
+cdf = A & C
+bch = A & B
+df = (A & C) - B
+c_only = A & B & C
+lmo = B - (A | C)
+
+print("h, i, j, k:", hijk)
+print("c, d, f:", cdf)
+print("b, c, h:", bch)
+print("d, f:", df)
+print("c:", c_only)
+print("l, m, o:", lmo)
